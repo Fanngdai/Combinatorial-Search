004c4438fca8de9e225979e20e43ad9235c996a7
Fanny Dai <fannydai@gmail.com>
2018-11-08 15:55:31 -0500

    modified word file

--- permutations.c
+++ permutations.c
@@ -5,18 +5,24 @@
 #include <limits.h>
 #include <string.h>
 
-// max possible next extensions
-#define MAXCANDIDATES   100
-// maximum solution size
-#define NMAX            100
 int **edges;
 int amt_vertex;
 int amt_edge;
 int *result;
 int bandwidth;
+int min;
 
-
-
+/*
+ * Checks for the max bandwidth in this permutation
+ *
+ * Loops through the edges and checks in the permutation to find the distance
+ * using the difference of index of the 2 verties. What you do first is find the
+ * first value of the two and keep it as an index. Then, you look for the second
+ * value of the edge and take the difference. If the difference is grreater than
+ * the bandwidth, we would return immediately! We only want to find a smaller
+ * bandwidth. We will continue until we have a bandwidth of less than or equal
+ * to what we originally had.
+ */
 int checkMax(int a[], int k){
 	int max = 0;
 	for(int e = 0; e < amt_edge; e++) {
@@ -27,6 +33,7 @@ int checkMax(int a[], int k){
 		for(int i=0; i<k; i++) {
 			if(index!=-1 && (a[i]==one || a[i]==two)) {
 				int tempMax = abs(index-i);
+				// Optimization
 				if(tempMax > bandwidth) {
 					return tempMax;
 				} else if(tempMax > max) {
@@ -41,8 +48,16 @@ int checkMax(int a[], int k){
 	return max;
 }
 
-
-
+/*
+ * Gets the max bandwidth of the current permutation and compare it with the
+ * minimum bandwidth which we previous got. If the current bandwidth is smaller
+ * than what we have, we will replace the result permutation along with a record
+ * of the new min bandwidth.
+ *
+ * You get the bandwidth of the current permutation and compare it to the
+ * previous. We want the smaller bandwidth therefore, if the current bandwidth
+ * is less than, we replace the bandwidth with this permutation.
+ */
 void process_solution(int a[]) {
 	int max = checkMax(a, amt_vertex);
 	if(max < bandwidth) {
@@ -51,27 +66,43 @@ void process_solution(int a[]) {
 	}
 }
 
-
-
+/*
+ * This function swaps the values of the two integer addresses.
+ */
 void swap(int *a, int *b) {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-
-
+/*
+ * Creates all the permutations. (Look at getMin() for the first Optimization)
+ * If we have a complete set, we will first check to see if this value was
+ * already used.
+ * A value (permutation) was already used if the last element is
+ * greater than the first element. Notice 123 is the same as 321. If we use 123,
+ * 321 will be eliminated. Note how the first index is greater than the last
+ * index. This will cut half the amount of time to process_solution.
+ * If the set is not complete, I first checked to see if the the subset we have
+ * have a bandwidth which is greater than the bandwidth we already have. Since
+ * we are searching for a smaller bandwidth, if the subset has a greater
+ * bandwidth, we would stop this subset from continuing and move to another
+ * subset.
+ *
+ */
 void permute(int *a,int i) {
-	if (amt_vertex-1 == i){
+	// Optimization - look at getMin()
+	if(bandwidth==min)
+		return;
+	else if (amt_vertex-1 == i){
+		// Optimization
 		if(a[0]<a[amt_vertex-1]) {
-			// for(int j=0; j<amt_vertex; j++)
-			// 	printf("%d ", a[j]);
-			// printf("\n");
 			process_solution(a);
 		}
 		return;
    }
 
+	// Optimization
    if(checkMax(a, i)>bandwidth)
 	   return;
 	for (int j = i; j<amt_vertex; j++) {
@@ -81,18 +112,46 @@ void permute(int *a,int i) {
 	}
 }
 
+/*
+ * I noticed that if a vertex is adjacent to x amount of vertex, then the max
+ * bandwidth would be x/2. I searched for the vertex with the most amount of
+ * degree. The value which I get (x/2) is the minimum value the bandwidth can
+ * be.
+ */
+void getMin() {
+	int vals[amt_vertex];
+	for(int i=0; i<amt_vertex; i++)
+		vals[i]=0;
+	for(int i=0; i<amt_edge; i++) {
+		int one = edges[i][0];
+		int two = edges[i][1];
+		vals[one-1]++;
+		vals[two-1]++;
+	}
+	min = 0;
+	for(int i=0; i<amt_vertex; i++) {
+		printf("%d ", vals[i]);
+		if(vals[i]>min)
+			min = vals[i];
+	}
+	min += 1;
+	min /= 2;
+}
 
-
+/*
+ * I do not calculate the time I spent reading the file and setting up my
+ * program since there are many ways of doing it such as hardcoding. Since
+ * everyone did it differently and some can be O(1) time, I chose to leave it
+ * out. My edges are in a 2d array which always has a column of 2 and a row of
+ * the amount of edges.
+ */
 int main() {
-	char *filename = "./Samples/g-t-13-12";
+	char *filename = "./Samples/g-t-8-7";
 	FILE *file = fopen(filename, "r");
 	if(file == NULL)
 		exit(0);
 
 	fscanf(file, "%d\n%d", &amt_vertex, &amt_edge);
-	// printf("%d\n%d\n", amt_vertex, amt_edge);
-	// printf("%d\n", amt_edge);
-
 	result = (int *)malloc(amt_vertex* sizeof(int));
 	edges = (int **)malloc(amt_edge*sizeof(int *));
 	for(int i=0; i<amt_edge; i++)
@@ -110,8 +169,6 @@ int main() {
 			edges[temp_amt_edge][1] = temp_to;
 		}
 		temp_amt_edge++;
-	  	// printf ("%d %d\n", amt_vertex, amt_edge);
-		// make sure the values are not ""
     }
 
 	int a[amt_vertex];
@@ -119,8 +176,8 @@ int main() {
 		a[i] = i+1;
 	clock_t start, end;
 	start = clock();
+	getMin();
 	permute(a, 0);
-	// backtrack(a,0);
 	end = clock();
 
 	printf("Filename: %s\n", filename);
@@ -128,89 +185,9 @@ int main() {
 	for(int i=0; i<amt_vertex; i++) {
 		printf("%d ", result[i]);
 	}
-	printf("\n");
+	printf("\nTotal time taken by CPU: %f\n", (double)(end-start)/CLOCKS_PER_SEC);
 
 	fclose(file);
-
-	// free(edges);
-	// free(result);
-   	printf("Total time taken by CPU: %f\n", (double)(end-start)/CLOCKS_PER_SEC);
+	free(edges);
+	free(result);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /*	What are possible elements of the next slot in the permutation?  */
-// void construct_candidates(int a[], int k, int c[], int *ncandidates) {
-// 	// What is not in the permutation?
-// 	int in_perm[NMAX] = {0};
-// 	// int in_perm[NMAX];
-// 	// for (int i=1; i<amt_vertex; i++) {
-// 	// 	in_perm[i] = 0;
-// 	// }
-//
-// 	for (int i=1; i<k; i++) {
-// 		in_perm[a[i]] = 1;
-// 	}
-//
-// 	*ncandidates = 0;
-// 	for(int i=1; i<=amt_vertex; i++) {
-// 		if (in_perm[i] == 0) {
-// 			c[(*ncandidates)++] = i;
-// 		}
-// 	}
-// }
-//
-//
-//
-// void backtrack(int a[], int k) {
-// 	// candidates for next position
-//     int c[MAXCANDIDATES];
-// 	// next position candidate count
-//     int ncandidates;
-//
-//     if (k==amt_vertex) {
-// 		if(a[1]<a[amt_vertex]) {
-// 			process_solution(a);
-// 		}
-// 	} else {
-// 		construct_candidates(a,++k,c,&ncandidates);
-//         for(int i=0; i<ncandidates; i++) {
-//             a[k] = c[i];
-//         	backtrack(a,k);
-//         }
-//     }
-// }
-//
-//
-// char *file[77] = { "g-bt-10-9", "g-bt-11-10", "g-bt-12-11", "g-bt-127-126",
-// 				"g-bt-13-12", "g-bt-14-13", "g-bt-15-14", "g-bt-16-15",
-// 				"g-bt-17-16", "g-bt-18-17", "g-bt-19-18", "g-bt-20-19",
-// 				"g-bt-63-62", "g-gg-100-180", "g-gg-12-17", "g-gg-15-22",
-// 				"g-gg-16-24", "g-gg-18-27", "g-gg-20-31", "g-gg-24-38",
-// 				"g-gg-25-40", "g-gg-28-45", "g-gg-30-49", "g-gg-33-52",
-// 				"g-gg-35-58", "g-gg-36-60", "g-gg-9-12", "g-gg-91-162",
-// 				"g-k-50-1225", "g-p-10-9", "g-p-100-99", "g-p-11-10",
-// 				"g-p-12-11", "g-p-13-12", "g-p-14-13", "g-p-15-14",
-// 				"g-p-16-15", "g-p-17-16", "g-p-18-17", "g-p-19-18",
-// 				"g-p-20-19", "g-p-25-24", "g-p-30-29", "g-p-40-39",
-// 				"g-p-50-49", "g-r-10-14", "g-r-10-9", "g-r-100-501",
-// 				"g-r-11-15", "g-r-12-15", "g-r-13-15", "g-r-15-24",
-// 				"g-r-18-46", "g-r-20-42", "g-r-25-83", "g-r-30-109",
-// 				"g-r-50-301", "g-r-7-7", "g-r-8-11", "g-r-8-4", "g-r-8-6",
-// 				"g-r-9-13", "g-t-10-9", "g-t-11-10", "g-t-12-11",
-// 				"g-t-13-12", "g-t-14-13", "g-t-15-14", "g-t-17-16",
-// 				"g-t-20-19", "g-t-25-24", "g-t-30-29", "g-t-35-34",
-// 				"g-t-40-39", "g-t-7-6", "g-t-8-7","g-t-9-8"};
